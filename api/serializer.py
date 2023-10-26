c906cd4c4bcf44b433d77a7c89f28e7ef943d875
Arunava <arunavapatra18@gmail.com>
2023-10-27 03:47:33 +0530

    Adding serializer for registration and login. Adding post() methods for user login in view.py

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 from accounts.models import CustomUser
 
-class CustomUserSerializer(ModelSerializer):
+# User login serializer (Read-only): Validate user login credentials and perform user login
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect credenetials!")
+        
+# User registration serializer(Write): User registration is performed
+class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        pass
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only' : True}}          # password field is made write-only to protect sensitive information
+    
+    # Override default create method. Creates a new CustomUser instance with the email, password provided. 
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+        return user
+    
--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+from .serializer import UserLoginSerializer, UserRegisterSerializer
 
 # Create your views here.
+class AuthLoginAPIView(APIView):
+    
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        
+        return Response({'token':token.key}, status=status.HTTP_200_OK)
+    
+class AuthLogoutAPIView(APIView):
+    # TODO
+    pass
